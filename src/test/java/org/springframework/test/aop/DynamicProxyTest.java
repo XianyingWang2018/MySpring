88bbe7571590ca24c406b5b8a6bda2d607282911
汪先应 <xiany.wang@sunyard.com>
2023-09-04 22:39:14 +0800

    mini-spring的cglib-dynamic-proxy

--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -5,6 +5,7 @@ import org.springframework.aop.AdvisedSupport;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.TargetSource;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.framework.CglibAopProxy;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
 import org.springframework.test.event.WorldServiceInterceptor;
 import org.springframework.test.service.HelloService;
@@ -27,6 +28,22 @@ public class DynamicProxyTest {
         advisedSupport.setMethodMatcher(methodMatcher);
 
         WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
+    @Test
+    public void testCglibDynamicProxy() throws Exception {
+        WorldService worldService = new WorldServiceImpl();
+
+        AdvisedSupport advisedSupport = new AdvisedSupport();
+        TargetSource targetSource = new TargetSource(worldService);
+        WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
+        MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))").getMethodMatcher();
+        advisedSupport.setTargetSource(targetSource);
+        advisedSupport.setMethodInterceptor(methodInterceptor);
+        advisedSupport.setMethodMatcher(methodMatcher);
+
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
         proxy.explode();
     }
 }
